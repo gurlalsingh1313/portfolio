e748d2e3a6dabb62b6379ac10d21fc238e9b22b4
gurlalsingh1313 <150024664+gurlalsingh1313@users.noreply.github.com>
2024-11-30 02:46:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-})
+  build: {
+    rollupOptions: {
+      external: ['@splinetool/react-spline'], // Ensure it's bundled
+    },
+  },
+});
